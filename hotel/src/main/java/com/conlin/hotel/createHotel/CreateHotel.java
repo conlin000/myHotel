a2cd48c4cb733d62045b290f8191ac1d883ecd06
Conlin <2567900371@qq.com>
2020-09-10 14:32:56 +0800

    配置了mysq，mybatis。创建了酒店数据库并且建表。

---
+++ hotel/src/main/java/com/conlin/hotel/createHotel/CreateHotel.java
@@ -0,0 +1,47 @@
+package com.conlin.hotel.createHotel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class CreateHotel {
+
+    @Resource
+    private HotelDao hotelDao;
+
+    @RequestMapping("/createHotel")
+    public void createHotel(){
+        Room room = new Room();
+        for (int i = 2; i < 9; i++) {
+
+            for (int j = 1; j < 11; j++) {
+
+                room.setRoomId(i * 100 + j);
+                room.setFloor(i);
+                room.setRoomNum(j);
+                room.setRoomName(i * 100 + j + "号房");
+                room.setRoomLevel(1);
+                room.setRoomType(1);
+                if ((i >= 3 && i < 6 || i == 7) && j >= 6) {
+                    room.setRoomType(2);
+                }
+                if (i >= 6) {
+                    room.setRoomLevel(2);
+                }
+                if (i == 8) {
+                    room.setRoomLevel(3);
+                    room.setRoomType(3);
+                }
+
+                // 写入数据库
+                hotelDao.hotel(room);
+                System.out.print(" |" + room.getRoomName() + "-" + room.getRoomLevel() + "-" + room.getRoomType() + "人房" + "| ");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ hotel/src/main/java/com/conlin/hotel/createHotel/Room.java
@@ -0,0 +1,61 @@
+package com.conlin.hotel.createHotel;
+
+import lombok.Data;
+
+@Data
+public class Room {
+    // 房间的id
+    private int roomId;
+
+    // 使用房间客户的id（可以多个房客）
+    private String userId;
+
+    // 房间名称
+    private String roomName;
+
+    // 楼层
+    private int floor;
+
+    // 房间号
+    private int roomNum;
+
+    // 房间介绍
+    private String roomContent;
+
+    // 房间级别：1-普通；2-豪华；3-总统套房
+    private int roomLevel;
+
+    // 房间类型：1-单人间；2-双人间；3-总统套房
+    private int roomType;
+
+    // 房间状态：0-不可用；1-空闲；2-已被预定；3-已被入住（用户不可见）
+    private int roomStatus;
+
+    // 被使用标志：0-被使用；1-未被使用
+    private int isUsed;
+
+    // 预定开始时间
+    private String bookTime;
+
+    // 预定结束时间
+    private String endTime;
+
+    // 入住时间
+    private String checkInTime;
+
+    // 退房时间
+    private String checkOutTime;
+
+    // 删除标志：0-未删除；1-已删除
+    private int isDeleted;
+
+    // 创建时间
+    private String gmtCreate;
+
+    // 修改时间
+    private String gmtModified;
+
+    // 版本号
+    private int version;
+
+}
